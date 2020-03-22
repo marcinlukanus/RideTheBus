87fbc625bf90adb8434a84f8e053acdf8e30ae9a
Marcin <mlukanus@gmail.com>
2020-03-21 23:07:05 -0500

    Binds each card to their proper image source

--- ridethebus/src/components/Game.vue
+++ ridethebus/src/components/Game.vue
@@ -4,10 +4,22 @@
             Cards
         </b-button>
         <div class="row justify-content-center mt-3">
-            <PlayingCard class="mx-3" />
-            <PlayingCard class="mx-3" />
-            <PlayingCard class="mx-3" />
-            <PlayingCard class="mx-3" />
+            <PlayingCard 
+                v-bind:imgsrc=cards.card1.image
+                class="mx-3" 
+            />
+            <PlayingCard 
+                v-bind:imgsrc=cards.card2.image
+                class="mx-3" 
+            />
+            <PlayingCard 
+                v-bind:imgsrc=cards.card3.image
+                class="mx-3" 
+            />
+            <PlayingCard 
+                v-bind:imgsrc=cards.card4.image
+                class="mx-3" 
+            />
         </div>
     </div>
 </template>
@@ -21,7 +33,12 @@ export default {
         PlayingCard
     },
     data: () => ({
-        cards: [],
+        cards: {
+            card1: {},
+            card2: {},
+            card3: {},
+            card4: {},
+        },
         gameStarted: false,
     }),
     methods: {
@@ -30,7 +47,10 @@ export default {
             await axios
                 .get('https://deckofcardsapi.com/api/deck/new/draw/?count=4')
                 .then(response => {
-                    this.cards = response.data.cards
+                    this.cards.card1 = response.data.cards[0]
+                    this.cards.card2 = response.data.cards[1]
+                    this.cards.card3 = response.data.cards[2]
+                    this.cards.card4 = response.data.cards[3]
                 })
                 .catch(error => {
                     console.log(error)
