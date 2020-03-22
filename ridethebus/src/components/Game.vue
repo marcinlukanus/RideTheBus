738117374ef251887723d89968f2434f95c05903
Marcin <mlukanus@gmail.com>
2020-03-22 12:33:49 -0500

    Players can now individually click each card to flip them

--- ridethebus/src/components/Game.vue
+++ ridethebus/src/components/Game.vue
@@ -1,23 +1,50 @@
 <template>
     <div class="container">
-        <b-button v-on:click="startGame()">
-            Cards
+        <b-button 
+            v-on:click="startGame()"
+            variant="success"
+        >
+            Draw Cards
         </b-button>
         <div class="row justify-content-center mt-3">
             <PlayingCard 
-                v-bind:imgsrc=cards.card1.image
+                v-if="!cardOneFlipped"
+                v-on:click.native="flipCard(0)"
                 class="mx-3" 
             />
             <PlayingCard 
-                v-bind:imgsrc=cards.card2.image
+                v-else
+                v-bind:imgsrc=cards[0].image
                 class="mx-3" 
             />
             <PlayingCard 
-                v-bind:imgsrc=cards.card3.image
+                v-if="!cardTwoFlipped"
+                v-on:click.native="flipCard(1)"
                 class="mx-3" 
             />
             <PlayingCard 
-                v-bind:imgsrc=cards.card4.image
+                v-else
+                v-bind:imgsrc=cards[1].image
+                class="mx-3" 
+            />
+            <PlayingCard 
+                v-if="!cardThreeFlipped"
+                v-on:click.native="flipCard(2)"
+                class="mx-3" 
+            />
+            <PlayingCard 
+                v-else
+                v-bind:imgsrc=cards[2].image
+                class="mx-3" 
+            />
+            <PlayingCard 
+                v-if="!cardFourFlipped"
+                v-on:click.native="flipCard(3)"
+                class="mx-3" 
+            />
+            <PlayingCard 
+                v-else
+                v-bind:imgsrc=cards[3].image
                 class="mx-3" 
             />
         </div>
@@ -33,28 +60,51 @@ export default {
         PlayingCard
     },
     data: () => ({
-        cards: {
-            card1: {},
-            card2: {},
-            card3: {},
-            card4: {},
-        },
+        cardOneFlipped: false,
+        cardTwoFlipped: false,
+        cardThreeFlipped: false,
+        cardFourFlipped: false,
+        cards: [
+            {},
+            {},
+            {},
+            {},
+        ],
         gameStarted: false,
     }),
     methods: {
+        flipCard(cardNum) {
+            switch(cardNum) {
+                case 0:
+                    this.cardOneFlipped = true
+                    break
+                case 1:
+                    this.cardTwoFlipped = true
+                    break
+                case 2:
+                    this.cardThreeFlipped = true
+                    break
+                case 3:
+                    this.cardFourFlipped = true
+                    break
+                default:
+                    break
+            }
+        },
         async startGame() {
             this.gameStarted = true
             await axios
                 .get('https://deckofcardsapi.com/api/deck/new/draw/?count=4')
                 .then(response => {
-                    this.cards.card1 = response.data.cards[0]
-                    this.cards.card2 = response.data.cards[1]
-                    this.cards.card3 = response.data.cards[2]
-                    this.cards.card4 = response.data.cards[3]
+                    this.cards[0] = response.data.cards[0]
+                    this.cards[1] = response.data.cards[1]
+                    this.cards[2] = response.data.cards[2]
+                    this.cards[3] = response.data.cards[3]
                 })
                 .catch(error => {
                     console.log(error)
                 })
+            this.cardOneFlipped = this.cardTwoFlipped = this.cardThreeFlipped = this.cardFourFlipped = false
             console.log(this.cards)
         }
     },
