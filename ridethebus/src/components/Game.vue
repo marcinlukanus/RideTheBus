bf542f5d7f441c87d1be2e9c0ba795c401951fea
Marcin <mlukanus@gmail.com>
2020-03-21 22:31:04 -0500

    Implements axios call to drawing intial 4 cards and sets up img src for PlayingCards

--- ridethebus/src/components/Game.vue
+++ ridethebus/src/components/Game.vue
@@ -1,32 +1,42 @@
 <template>
-    <div class="container-fluid">
-        <PlayingCard />
-        <PlayingCard />
-        <PlayingCard />
-        <PlayingCard />
+    <div class="container">
+        <b-button v-on:click="startGame()">
+            Cards
+        </b-button>
+        <div class="row justify-content-center mt-3">
+            <PlayingCard class="mx-3" />
+            <PlayingCard class="mx-3" />
+            <PlayingCard class="mx-3" />
+            <PlayingCard class="mx-3" />
+        </div>
     </div>
 </template>
 
 <script>
 import PlayingCard from './PlayingCard.vue'
+import axios from 'axios'
 
 export default {
     components: {
         PlayingCard
     },
     data: () => ({
+        cards: [],
         gameStarted: false,
-
     }),
     methods: {
-        restartGame() {
-        },
-        startGame() {
+        async startGame() {
             this.gameStarted = true
+            await axios
+                .get('https://deckofcardsapi.com/api/deck/new/draw/?count=4')
+                .then(response => {
+                    this.cards = response.data.cards
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            console.log(this.cards)
         }
-    }
+    },
 }
 </script>
-
-<style scoped>
-</style>
--- ridethebus/src/components/PlayingCard.vue
+++ ridethebus/src/components/PlayingCard.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Card_back_06.svg/209px-Card_back_06.svg.png" />
+        <img :src="imgsrc" />
     </div>
 </template>
 
 <script>
 export default {
     props: {
-        image: String
+        imgsrc: {
+            type: String,
+            default() {
+                return 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Card_back_06.svg/209px-Card_back_06.svg.png'
+            }
+        }
     },
     data: function() {
         return {
-            imgsrc: this.image
         }
     }
 }
