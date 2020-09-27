4f5f4015bd672ab2b9729ca952d9af8085ab871c
unknown <mlukanus@gmail.com>
2020-09-26 19:05:10 -0500

    Updates Google Play image to open in new tab

--- src/components/AppDownloads.vue
+++ src/components/AppDownloads.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="justify-content-center">
-        <a href='https://play.google.com/store/apps/details?id=com.ridethebus&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+        <a 
+            href='https://play.google.com/store/apps/details?id=com.ridethebus&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'
+            target="_blank"    
+        >
             <img 
                 alt='Get it on Google Play'
                 src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'
